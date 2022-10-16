7424c11e39662f955f12a7b16314aa4850ee7695
expguy <t2110gh.bxduc@aptech.vn>
2022-10-16 22:26:03 +0700

    test

--- resources/views/livewire/home-component.blade.php
+++ resources/views/livewire/home-component.blade.php
@@ -115,199 +115,201 @@
   <!-- shop section -->
 
   <section class="shop_section layout_padding">
-        <div class="container">
-            <div class="heading_container heading_center">
-                <h2>
-                    Apple
-                </h2>
-            </div>
-            <div class="row">
-                @foreach($productsApple as $productApple)
-                <div class="col-sm-6 col-md-4 col-xl-3">
-                    <div class="box shadow productBox">
-                        <a href="">
-                            <div class="img-box">
-                                <img src="{{ $productApple->Featured }}" alt="">
-                            </div>
-                            <div class="detail-box d-flex flex-column justify-content-center align-items-center">
-                                <h6>
-                                    {{ $productApple->ProductName }}
-                                </h6>
-                                <h6>
-                                    Price:
-                                    <span>
-                                        $ {{ $productApple->Price }}
-                                    </span>
-                                </h6>
-                            </div>
-                            <div class="new">
-                                <span>
-                                    New
-                                </span>
-                            </div>
-                        </a>
-                    </div>
-                </div>
-                @endforeach
-            </div>
-            <div class="d-flex justify-content-center align-items-center mt-5"><a href="#" class="btn btn-warning fw-bold text-white shadow">View All</a></div>
+    <div class="container">
+      <div class="heading_container heading_center">
+        <h2>
+          Apple
+        </h2>
+      </div>
+      <div class="row">
+        @foreach($productsApple as $productApple)
+        <div class="col-sm-6 col-md-4 col-xl-3">
+          <div class="box shadow productBox">
+            <a href="">
+              <div class="img-box">
+                <img src="{{ $productApple->Featured }}" alt="">
+              </div>
+              <div class="detail-box d-flex flex-column justify-content-center align-items-center">
+                <h6>
+                  {{ $productApple->ProductName }}
+                </h6>
+                <h6>
+                  Price:
+                  <span>
+                    $ {{ $productApple->Price }}
+                  </span>
+                </h6>
+              </div>
+              <div class="new">
+                <span>
+                  New
+                </span>
+              </div>
+            </a>
+          </div>
         </div>
-        <div class="container mt-5">
-            <div class="heading_container heading_center">
-                <h2>
-                Samsung
-                </h2>
-            </div>
-            <div class="row">
-                @foreach($productsSamsung as $productSamsung)
-                <div class="col-sm-6 col-md-4 col-xl-3">
-                    <div class="box shadow productBox">
-                        <a href="">
-                            <div class="img-box">
-                                <img src="{{ $productSamsung->Featured }}" alt="">
-                            </div>
-                            <div class="detail-box d-flex flex-column justify-content-center align-items-center">
-                                <h6>
-                                    {{ $productSamsung->ProductName }}
-                                </h6>
-                                <h6>
-                                    Price:
-                                    <span>
-                                        $ {{ $productSamsung->Price }}
-                                    </span>
-                                </h6>
-                            </div>
-                            <div class="new">
-                                <span>
-                                    New
-                                </span>
-                            </div>
-                        </a>
-                    </div>
-                </div>
-                @endforeach
-            </div>
-            <div class="d-flex justify-content-center align-items-center mt-5 "><a href="#" class="btn btn-warning fw-bold text-white shadow">View All</a></div>
+        @endforeach
+      </div>
+      <div class="d-flex justify-content-center align-items-center mt-5"><a href="#" class="btn btn-warning fw-bold text-white shadow">View All</a></div>
+    </div>
+    <div class="container mt-5">
+      <div class="heading_container heading_center">
+        <h2>
+          Samsung
+        </h2>
+      </div>
+      <div class="row">
+        @foreach($productsSamsung as $productSamsung)
+        <div class="col-sm-6 col-md-4 col-xl-3">
+          <div class="box shadow productBox">
+            <a href="">
+              <div class="img-box">
+                <img src="{{ $productSamsung->Featured }}" alt="">
+              </div>
+              <div class="detail-box d-flex flex-column justify-content-center align-items-center">
+                <h6>
+                  {{ $productSamsung->ProductName }}
+                </h6>
+                <h6>
+                  Price:
+                  <span>
+                    $ {{ $productSamsung->Price }}
+                  </span>
+                </h6>
+              </div>
+              <div class="new">
+                <span>
+                  New
+                </span>
+              </div>
+            </a>
+          </div>
         </div>
-        <div class="container mt-5">
-            <div class="heading_container heading_center">
-                <h2>
-                Xiaomi
-                </h2>
-            </div>
-            <div class="row">
-                @foreach($productsXiaomi as $productXiaomi)
-                <div class="col-sm-6 col-md-4 col-xl-3">
-                    <div class="box shadow productBox">
-                        <a href="">
-                            <div class="img-box">
-                                <img src="{{ $productXiaomi->Featured }}" alt="">
-                            </div>
-                            <div class="detail-box d-flex flex-column justify-content-center align-items-center">
-                                <h6>
-                                    {{ $productXiaomi->ProductName }}
-                                </h6>
-                                <h6>
-                                    Price:
-                                    <span>
-                                        $ {{ $productXiaomi->Price }}
-                                    </span>
-                                </h6>
-                            </div>
-                            <div class="new">
-                                <span>
-                                    New
-                                </span>
-                            </div>
-                        </a>
-                    </div>
+        @endforeach
+      </div>
+      <div class="d-flex justify-content-center align-items-center mt-5 "><a href="#" class="btn btn-warning fw-bold text-white shadow">View All</a></div>
+    </div>
+    <div class="container mt-5">
+      <div class="heading_container heading_center">
+        <h2>
+          Xiaomi
+        </h2>
+      </div>
+      <div class="d-flex justify-content-center align-items-center shop-slick">
+        
+          @foreach($productsXiaomi as $productXiaomi)
+          <div class="col-sm-6 col-md-4 col-xl-3 mb-5">
+            <div class="box shadow productBox">
+              <a href="">
+                <div class="img-box">
+                  <img src="{{ $productXiaomi->Featured }}" alt="">
                 </div>
-                @endforeach
+                <div class="detail-box d-flex flex-column justify-content-center align-items-center">
+                  <h6>
+                    {{ $productXiaomi->ProductName }}
+                  </h6>
+                  <h6>
+                    Price:
+                    <span>
+                      $ {{ $productXiaomi->Price }}
+                    </span>
+                  </h6>
+                </div>
+                <div class="new">
+                  <span>
+                    New
+                  </span>
+                </div>
+              </a>
             </div>
-            <div class="d-flex justify-content-center align-items-center mt-5 "><a href="#" class="btn btn-warning fw-bold text-white shadow">View All</a></div>
+          </div>
+          @endforeach
+        
+      </div>
+      <div class="d-flex justify-content-center align-items-center"><a href="#" class="btn btn-warning fw-bold text-white shadow">View All</a></div>
+    </div>
+  </section>
+
+  <!-- end shop section -->
+
+  <!-- about section -->
+
+  <section class="about_section layout_padding">
+    <div class="container  ">
+      <div class="row">
+        <div class="col-md-6 col-lg-5 ">
+          <div class="img-box">
+            <img src="img/about-img.png" alt="">
+          </div>
         </div>
-    </section>
-
-    <!-- end shop section -->
-
-    <!-- about section -->
-
-    <section class="about_section layout_padding">
-        <div class="container  ">
-            <div class="row">
-                <div class="col-md-6 col-lg-5 ">
-                    <div class="img-box">
-                        <img src="img/about-img.png" alt="">
-                    </div>
-                </div>
-                <div class="col-md-6 col-lg-7">
-                    <div class="detail-box">
-                        <div class="heading_container">
-                            <h2>
-                                About Us
-                            </h2>
-                        </div>
-                        <p>
-                            There are many variations of passages of Lorem Ipsum available, but the majority have
-                            suffered alteration
-                            in some form, by injected humour, or randomised words which don't look even slightly
-                            believable. If you
-                            are going to use a passage of Lorem Ipsum, you need to be sure there isn't anything
-                            embarrassing hidden in
-                            the middle of text. All
-                        </p>
-                        <a href="">
-                            Read More
-                        </a>
-                    </div>
-                </div>
+        <div class="col-md-6 col-lg-7">
+          <div class="detail-box">
+            <div class="heading_container">
+              <h2>
+                About Us
+              </h2>
             </div>
+            <p>
+              There are many variations of passages of Lorem Ipsum available, but the majority have
+              suffered alteration
+              in some form, by injected humour, or randomised words which don't look even slightly
+              believable. If you
+              are going to use a passage of Lorem Ipsum, you need to be sure there isn't anything
+              embarrassing hidden in
+              the middle of text. All
+            </p>
+            <a href="">
+              Read More
+            </a>
+          </div>
         </div>
-    </section>
+      </div>
+    </div>
+  </section>
 
-    <!-- end about section -->
+  <!-- end about section -->
 
-    <!-- contact section -->
+  <!-- contact section -->
 
-    <section class="contact_section mb-5">
-        <div class="container">
-            <div class="row">
-                <div class="col-md-6">
-                    <div class="form_container">
-                        <div class="heading_container">
-                            <h2>
-                                Contact Us
-                            </h2>
-                        </div>
-                        <form action="">
-                            <div>
-                                <input type="text" placeholder="Full Name " />
-                            </div>
-                            <div>
-                                <input type="email" placeholder="Email" />
-                            </div>
-                            <div>
-                                <input type="text" placeholder="Phone number" />
-                            </div>
-                            <div>
-                                <input type="text" class="message-box" placeholder="Message" />
-                            </div>
-                            <div class="d-flex ">
-                                <button>
-                                    SEND
-                                </button>
-                            </div>
-                        </form>
-                    </div>
-                </div>
-                <div class="col-md-6">
-                    <div class="img-box">
-                        <img src="img/contact-img.jpg" alt="">
-                    </div>
-                </div>
+  <section class="contact_section mb-5">
+    <div class="container">
+      <div class="row">
+        <div class="col-md-6">
+          <div class="form_container">
+            <div class="heading_container">
+              <h2>
+                Contact Us
+              </h2>
             </div>
+            <form action="">
+              <div>
+                <input type="text" placeholder="Full Name " />
+              </div>
+              <div>
+                <input type="email" placeholder="Email" />
+              </div>
+              <div>
+                <input type="text" placeholder="Phone number" />
+              </div>
+              <div>
+                <input type="text" class="message-box" placeholder="Message" />
+              </div>
+              <div class="d-flex ">
+                <button>
+                  SEND
+                </button>
+              </div>
+            </form>
+          </div>
         </div>
-    </section>
+        <div class="col-md-6">
+          <div class="img-box">
+            <img src="img/contact-img.jpg" alt="">
+          </div>
+        </div>
+      </div>
+    </div>
+  </section>
 
-    <!-- end contact section -->
+  <!-- end contact section -->
 </div>
