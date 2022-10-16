5f5ccb3c223de11b631c263a9f240b8245bdea86
expguy <t2110gh.bxduc@aptech.vn>
2022-10-16 22:27:45 +0700

    test

--- app/Http/Livewire/HomeComponent.php
+++ app/Http/Livewire/HomeComponent.php
@@ -12,15 +12,15 @@ class HomeComponent extends Component
     $productsApple = DB::table('product')
     ->join('size', 'product.id', '=', 'size.product_id')
     ->join('category', 'product.category_id', '=', 'category.id')
-    ->select('product.*', 'size.Price')->where('category.id','=','1')->limit(4)->get();
+    ->select('product.*', 'size.Price')->where('category.id','=','1')->get();
     $productsSamsung = DB::table('product')
     ->join('size', 'product.id', '=', 'size.product_id')
     ->join('category', 'product.category_id', '=', 'category.id')
-    ->select('product.*', 'size.Price')->where('category.id','=','2')->limit(4)->get();
+    ->select('product.*', 'size.Price')->where('category.id','=','2')->get();
     $productsXiaomi = DB::table('product')
     ->join('size', 'product.id', '=', 'size.product_id')
     ->join('category', 'product.category_id', '=', 'category.id')
-    ->select('product.*', 'size.Price')->where('category.id','=','3')->limit(4)->get();
+    ->select('product.*', 'size.Price')->where('category.id','=','3')->get();
     return view('livewire.home-component', compact('productsApple','productsSamsung','productsXiaomi'))->layout('layouts.base');
   }
 }
